6cd3f4dfa5a3cc11b4c9719b2ceab2330d5ed6dc
hydrol0x <mrjacobry@gmail.com>
2022-07-18 17:17:07 -0400

    Import edit in ToDO component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import "../App.css" 
-import Delete from './Delete';
+import React from "react";
+import "../App.css";
+import Delete from "./Delete";
 
-const ToDo = ({todo, handleToggle, handleDelete}) => {
-    const handleClick = (e) => {
-        e.preventDefault();
-        console.log(e.currentTarget)
-        handleToggle(e.currentTarget.id);
-    }
-    console.log(handleDelete)
-    return (
-        <div id={todo.id} onClick={handleClick} className={todo.complete ? "strike" : ""}>
-            <Delete id={todo.id} handleDelete={handleDelete} />
-            <p>{todo.task}</p>
-        </div>
-    );
+const ToDo = ({ todo, handleToggle, handleDelete }) => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    handleToggle(e.currentTarget.id);
+  };
+  return (
+    <div>
+      <Delete id={todo.id} handleDelete={handleDelete} />
+      <Edit id={todo.id} handleEdit={handleEdit} />
+      <p
+        id={todo.id}
+        onClick={handleClick}
+        className={todo.complete ? "strike" : ""}
+      >
+        {todo.task}
+      </p>
+    </div>
+  );
 };
- 
+
 export default ToDo;

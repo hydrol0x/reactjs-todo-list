3e6217f591923873551d2b1d212b1a5a79f9d6a2
hydrol0x <mrjacobry@gmail.com>
2022-07-18 16:57:28 -0400

    Reads from DB

--- src/App.js
+++ src/App.js
@@ -1,34 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import data from "./data.json";
+
+// Firestore DB
+import { collection, doc, getDocs } from "firebase/firestore";
+import { db } from "./firestore";
+
 //components
 import Header from "./components/Header";
 import ToDoList from "./components/ToDoList";
- 
-import './App.css';
- 
+import ToDoForm from "./components/ToDoForm";
+
+import "./App.css";
+
 function App() {
+  const [toDoList, setToDoList] = useState([]);
+
+  const tasksCollectionRef = collection(db, "tasks");
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await getDocs(tasksCollectionRef);
+      setToDoList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getTasks();
+  }, []);
+
   const handleToggle = (id) => {
-    let mapped = toDoList.map(task => {
-      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    let mapped = toDoList.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, complete: !task.complete }
+        : { ...task };
     });
     setToDoList(mapped);
-  }
+  };
 
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     const removeItem = toDoList.filter((task) => {
       return task.id !== id;
     });
     setToDoList(removeItem);
-  }
+  };
 
-  const [ toDoList, setToDoList ] = useState(data);
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [
+      ...copy,
+      { id: toDoList.length + 1, task: userInput, complete: false },
+    ];
+    setToDoList(copy);
+  };
 
   return (
-   <div className="App">
-     <Header />
-     <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleDelete={handleDelete}/>
-   </div>
- );
+    <div className="App">
+      <Header />
+      <ToDoForm addTask={addTask} />
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleDelete={handleDelete}
+      />
+    </div>
+  );
 }
- 
+
 export default App;
